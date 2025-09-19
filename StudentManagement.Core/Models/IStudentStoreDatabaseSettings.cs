4f8b961f16b8b2930eebb80e5f0641d24849fa50
Amulya <Amulya@cmartsolutions.com>
2025-09-19 15:36:21 +0530

    Restructured: moved Models to Core project

--- StudentManagement.Core/Models/IStudentStoreDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentManagement.Core.Models
-{
-    public interface IStudentStoreDatabaseSettings
-    {
-        string StudentCoursesCollectionName { get; set; }
-        string ConnectionString { get; set; }
-        string DatabaseName { get; set; }
-    }
-}
